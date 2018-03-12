69813446dd0551242d547138e9c30894d6bdc336
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-03-12 15:57:14 +0800

    服务器代SOCKET的多人多次发送和接受

--- MyJungleWarServer/Program.cs
+++ MyJungleWarServer/Program.cs
@@ -10,6 +10,12 @@ namespace MyJungleWarServer
     {
         public static void Main(string[] args)
         {
+            Program p = new Program();
+            p.StartServerAsync();
+        }
+
+        private void StartServerSync()
+        {
             Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             //本机IP：192.168.0.120     电脑内部IP：127.0.0.1
             //IPAddress xxx.xxx.xxx.xxx IPEndPoint xxx.xxx.xxx.xxx:port
@@ -33,6 +39,76 @@ namespace MyJungleWarServer
 
             clientSocket.Close();
             serverSocket.Close();
+
+            while (true)
+            {
+                Console.ReadLine();
+            }
+        }
+
+
+        private byte[] asyncDataBuffer = new byte[1024];
+        private void StartServerAsync()
+        {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //本机IP：192.168.0.120     电脑内部IP：127.0.0.1
+            //IPAddress xxx.xxx.xxx.xxx IPEndPoint xxx.xxx.xxx.xxx:port
+            //IPAddress ipAddress = new IPAddress(new byte[] { 127, 0, 0, 1 });//不推荐
+            IPAddress ipAddress = IPAddress.Parse("192.168.0.120");
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, 6666);
+            serverSocket.Bind(ipEndPoint);
+            serverSocket.Listen(0);//开始监听端口号   设置为0 数量没有限制
+
+            serverSocket.BeginAccept(AcceptCallBack, serverSocket);
+
+            while (true)
+            {
+                Console.ReadLine();
+            }
+        }
+
+        private void AcceptCallBack(IAsyncResult ar)
+        {
+            Socket serverSocket = ar.AsyncState as Socket;
+            Socket clientSocket = serverSocket.EndAccept(ar);
+
+            //向客户端发送消息
+            string msg = "Hello World!雷侯啊，啊油OK！";
+            var data = Encoding.UTF8.GetBytes(msg);
+            clientSocket.Send(data);
+
+            asyncDataBuffer = new byte[1024];
+            clientSocket.BeginReceive(asyncDataBuffer, 0, 1024, SocketFlags.None, ReceiveCallBack, clientSocket);
+
+            serverSocket.BeginAccept(AcceptCallBack, serverSocket);
+        }
+
+        private void ReceiveCallBack(IAsyncResult ar)
+        {
+            Socket clientSocket = null;
+            try
+            {
+                clientSocket = (Socket)ar.AsyncState;
+                int count = clientSocket.EndReceive(ar);
+                if (count <= 0)
+                {
+                    clientSocket.Close();
+                }
+                else
+                {
+                    string msg = Encoding.UTF8.GetString(asyncDataBuffer, 0, count);
+                    Console.WriteLine(msg);
+                    clientSocket.BeginReceive(asyncDataBuffer, 0, 1024, SocketFlags.None, ReceiveCallBack, clientSocket);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                if (clientSocket != null)
+                {
+                    clientSocket.Close();
+                }
+            }
         }
     }
 }
