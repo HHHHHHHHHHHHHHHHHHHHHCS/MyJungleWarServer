2802c1ad6403791407ac8b271e3c6eeb901eac21
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-03-26 11:22:46 +0800

    数据库结构更新

--- SQL/myjunglewarsql.sql
+++ SQL/myjunglewarsql.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 2018-03-14 07:57:55
+-- Generation Time: 2018-03-26 05:21:56
 -- 服务器版本： 10.1.24-MariaDB
 -- PHP Version: 7.1.6
 
@@ -40,7 +40,21 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id`, `username`, `password`) VALUES
 (1, 'aaa', 'aaa'),
-(2, 'bbb', 'bbb');
+(2, 'bbb', 'bbb'),
+(3, 'ccc', 'ccc'),
+(4, 'ddd', 'ddd');
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `userdata`
+--
+
+CREATE TABLE `userdata` (
+  `username` varchar(20) NOT NULL,
+  `totalcount` int(11) NOT NULL,
+  `wincount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -54,6 +68,13 @@ ALTER TABLE `user`
   ADD UNIQUE KEY `id` (`id`);
 
 --
+-- Indexes for table `userdata`
+--
+ALTER TABLE `userdata`
+  ADD PRIMARY KEY (`username`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
 -- 在导出的表使用AUTO_INCREMENT
 --
 
@@ -61,7 +82,17 @@ ALTER TABLE `user`
 -- 使用表AUTO_INCREMENT `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- 限制导出的表
+--
+
+--
+-- 限制表 `userdata`
+--
+ALTER TABLE `userdata`
+  ADD CONSTRAINT `username_fk` FOREIGN KEY (`username`) REFERENCES `user` (`username`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
